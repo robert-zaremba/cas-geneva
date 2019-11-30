9be933add67bd669fd96c68fbd2e31757493bcfa
Robert Zaremba <robert@zaremba.ch>
2019-11-30 11:12:53 +0100

    version - MetaCoin with admin functions

--- contracts/MetaCoin.sol
+++ contracts/MetaCoin.sol
@@ -8,9 +8,11 @@ import "./ConvertLib.sol";
 // token, see: https://github.com/ConsenSys/Tokens. Cheers!
 
 contract MetaCoin {
-	mapping (address => uint) balances;
+	mapping (address => uint256) balances;
+    address public owner;
+    uint256 public convertionRatio = 2;
 
-	event Transfer(address indexed _from, address indexed _to, uint256 _value);
+	event Transfer(address indexed from, address indexed to, uint256 value);
 
     // 1. let's define an owner
     // 2. set owner in the constructor
@@ -22,23 +24,44 @@ contract MetaCoin {
     //    -- this is a helper function to be used in moveCoins and sendCoin
     // 7. Understand the differance between tx.origing and msg.sender
 
-	constructor() public {
+	constructor(address _owner) public {
+        owner = _owner;
 		balances[tx.origin] = 10000;
 	}
 
-	function sendCoin(address receiver, uint amount) public returns(bool sufficient) {
-		if (balances[msg.sender] < amount) return false;
-		balances[msg.sender] -= amount;
-		balances[receiver] += amount;
-		emit Transfer(msg.sender, receiver, amount);
+    function setOwner(address _newOwner) public {
+        require (msg.sender == owner, "E001");
+        owner = _newOwner;
+    }
+
+    function setConvertionRatio(uint256 _newRatio) public {
+        require (msg.sender == owner);
+        convertionRatio = _newRatio;
+    }
+
+    function _transfer(address _from, address _to, uint256 _amount) internal {
+        require(balances[_from] >= _amount);
+        balances[_to] += _amount;
+        balances[_from] -= _amount;
+		emit Transfer(_from, _to, _amount);
+    }
+
+    function moveCoins(address _from, address _to, uint256 _amount) external {
+        require (msg.sender == owner);
+        _transfer(_from, _to, _amount);
+    }
+
+
+	function sendCoin(address receiver, uint256 amount) public returns(bool) {
+        _transfer(msg.sender, receiver, amount);
 		return true;
 	}
 
-	function getBalanceInEth(address addr) public view returns(uint){
-		return ConvertLib.convert(getBalance(addr),2);
+	function getBalanceInEth(address addr) public view returns(uint256){
+		return ConvertLib.convert(getBalance(addr), convertionRatio);
 	}
 
-	function getBalance(address addr) public view returns(uint) {
+	function getBalance(address addr) public view returns(uint256) {
 		return balances[addr];
 	}
 }
